724a6eb78aac24ccf41d1a35f6fcc77b178d7b7e
jackDante <44863491+jackDante@users.noreply.github.com>
2022-11-09 20:02:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+multi
+riga
+"""
+
+def concatenazionestringhe(x, y):
+    x = "Pino____!!!"
+    print(x + y)
+
+
+def print_funzione():
+    x = "Hi"  # string
+    x = 10  # int
+    x = True  # boolean
+    x = 2.5  # float
+    x = x + 20
+#stampa a video la variabile x
+    print(x)
+    print(type(x))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_funzione()
+    concatenazionestringhe("Ciao   ", "icgghvghkvkgvui!")
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
